0ff5e1fdd52375f1863eb4209fb1d4e6d2a5d575
Zachary Hunt <=>
2020-10-17 22:05:17 -0500

    Reversion

--- app.py
+++ app.py
@@ -352,5 +352,11 @@ def page_not_found(e):
 login_manager.init_app(app)
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
 	app.run(host="0.0.0.0", debug=True)
 
+=======
+	pass
+	# app.run(host="0.0.0.0", debug=True)
+	# app.run(host='0.0.0.0', port=80)
+>>>>>>> Stashed changes
